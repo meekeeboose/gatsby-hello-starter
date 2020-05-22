5d0b2161ef25e04690930dc1c5244127080269fc
mediavil <meekeboose@gmail.com>
2020-05-22 21:56:26 +0800

    created layout component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,20 +1,17 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Header from "./components/header"
-import Footer from "./components/footer"
+import Layout from "./components/layout"
 
 const AboutPage = () => {
   return (
-    <div>
-      <Header />
+    <Layout>
       <h1>About me</h1>
       <p>I am who I am.</p>
       <p>
         Need a developer? <Link to="/contact">Contact me</Link>
       </p>
-      <Footer />
-    </div>
+    </Layout>
   )
 }
 
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,10 @@
 import React from "react"
 
-import Header from "./components/header"
-import Footer from "./components/footer"
+import Layout from "./components/layout"
 
 const ContactPage = () => {
   return (
-    <div>
-      <Header />
+    <Layout>
       <h1> Contact </h1>
       <p>
         The best way to reach me is via
@@ -14,8 +12,7 @@ const ContactPage = () => {
           Facebook
         </a>
       </p>
-      <Footer />
-    </div>
+    </Layout>
   )
 }
 
